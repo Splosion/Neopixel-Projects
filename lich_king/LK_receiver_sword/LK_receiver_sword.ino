a8869871ccc60bdd18c1b279716bd3476990cbc7
Splosion <mastertarus@gmail.com>
2020-05-21 19:16:23 +0100

    turn psuedocode into arduino code first try

--- lich_king/LK_receiver_sword/LK_receiver_sword.ino
+++ lich_king/LK_receiver_sword/LK_receiver_sword.ino
@@ -26,6 +26,23 @@ RF24 radio(10, 9); // NRF24L01 used SPI pins + Pin 9 and 10 on the UNO
 
 const uint64_t pipe = 0xE6E6E6E6E6E6; // Needs to be the same for communicating between 2 NRF24L01
 
+long previousButtonPress = 0;
+long currentButtonPress = 0;
+
+int blue[3] = {54, 245, 255};
+int orange[3] = {255, 45, 0};
+int noColour[3] = {0, 0, 0};
+
+int incrementRed = 0;
+int incrementGreen = 0;
+int incrementBlue = 0;
+int currentColour[3]{0, 0, 0};
+
+bool positiveIncrement = true;
+bool fastAnimate = false;
+bool buttonColour = false;
+int currentLED = 0;
+
 void setup(void)
 {
 
@@ -56,23 +73,96 @@ void loop(void)
     while (radio.available())
     {
         radio.read(ReceivedMessage, 1); // Read information from the NRF24L01
-
+        currentButtonPress = millis();
         if (ReceivedMessage[0] == 111) // Indicates switch is pressed
         {
-            for (int x = 0; x != NUM_LEDS; x++)
+            if (currentButtonPress - previousButtonPress >= 5000)
             {
-                leds[x].setRGB(255, 54, 0);
-                FastLED.show();
+                if (!buttonColour)
+                {
+                    previousButtonPress = millis();
+                    fastAnimate = true;
+                    buttonColour = true;
+                    memcpy(currentColour, noColour, 3);
+                }
+                else
+                {
+                    previousButtonPress = millis();
+                    fastAnimate = true;
+                    buttonColour = false;
+                    memcpy(currentColour, noColour, 3);
+                }
             }
+        }
+        if (buttonColour && positiveIncrement)
+        {
+            animate(currentColour, orange);
+        }
+        else if (positiveIncrement)
+        {
+            animate(currentColour, blue);
         }
         else
         {
-            for (int x = 0; x != NUM_LEDS; x++)
-            {
-                leds[x].setRGB(54, 245, 255);
-                FastLED.show();
-            }
+            animate(currentColour, noColour);
         }
         delay(10);
+    }
+}
+
+void animate(int colour[3], int targetColour[3])
+{
+    if (incrementRed == 0 && incrementGreen == 0 && incrementBlue == 0)
+    {
+        incrementRed = (targetColour[0] - colour[0]) / 5;
+        incrementGreen = (targetColour[1] - colour[1]) / 5;
+        incrementBlue = (targetColour[2] - colour[2]) / 5;
+        if (fastAnimate)
+        {
+            incrementRed = incrementRed * 2;
+            incrementGreen = incrementGreen * 2;
+            incrementBlue = incrementBlue * 2;
+        }
+    }
+    if (positiveIncrement)
+    {
+        currentColour[0] = min(255, colour[0] + incrementRed);
+        currentColour[1] = min(255, colour[1] + incrementGreen);
+        currentColour[2] = min(255, colour[2] + incrementBlue);
+        leds[currentLED].setRGB(currentColour[0], currentColour[2], currentColour[2]);
+        leds[currentLED + 5].setRGB(currentColour[0], currentColour[2], currentColour[2]);
+        FastLED.show();
+        if (currentColour == targetColour)
+        {
+            positiveIncrement = false;
+
+            incrementRed = 0;
+            incrementGreen = 0;
+            incrementBlue = 0;
+        }
+        if (fastAnimate)
+        {
+            fastAnimate = false;
+        }
+    }
+    else
+    {
+        currentColour[0] = colour[0] - incrementRed;
+        currentColour[1] = colour[1] - incrementGreen;
+        currentColour[2] = colour[2] - incrementBlue;
+        leds[currentLED].setRGB(currentColour[0], currentColour[2], currentColour[2]);
+        leds[currentLED + 5].setRGB(currentColour[0], currentColour[2], currentColour[2]);
+        for (int x = 0; x != NUM_LEDS; x++)
+        {
+            leds[x].setRGB(0, 0, 0);
+            FastLED.show();
+        }
+        if (currentColour == noColour)
+        {
+            positiveIncrement = true;
+            incrementRed = 0;
+            incrementGreen = 0;
+            incrementBlue = 0;
+        }
     }
 }
--- neopixel_moving_breathe/neopixel_moving_breathe.ino
+++ neopixel_moving_breathe/neopixel_moving_breathe.ino
@@ -48,6 +48,21 @@ void darken() {
   delay(1500);
 }
 
+void lighten() {
+  
+  uint16_t i, j;
+
+  for (j = 255; j > 255; j++) {
+    for (i = 0; i < strip.numPixels(); i++) {
+      strip.setPixelColor(i, 0, j, j);
+    }
+    strip.show();
+    delay(10);
+    
+  }
+  delay(1500);
+}
+
 const byte extra = 60;  // how many ahead, and behind, to light up
 
 // Pause = delay between transitions
@@ -74,20 +89,3 @@ void ledTrail(int pause, byte Rf, byte Gf, byte Bf, byte Rb, byte Gb, byte Bb) {
     delay(pause);
   }
 }
-def fade(self, length,color, currentPixel):
-        for ahead in range(0,length,1):
-            if currentPixel+ahead <= self.pixels.numPixels()-1:
-                divisor = ((length-ahead)/(length+1))
-                self.pixels.setPixelColor(currentPixel+ahead%self.pixels.numPixels(),self.pixels.Color(color[0]*divisor,color[1]*divisor,color[2]*divisor))
-    def inverseFade(self, length,color, currentPixel, end):
-        diff = self.pixels.numPixels()-currentPixel
-        if self.pixels.numPixels()-currentPixel<length:
-            length=diff
-        for ahead in range(1,length+1,1):
-            divisor = ((ahead)/(length+1))
-            print(diff)
-            print(currentPixel)
-            print(divisor)
-            print((currentPixel+self.pixels.numPixels()-ahead)%self.pixels.numPixels())
-            if (currentPixel+self.pixels.numPixels()-ahead)%self.pixels.numPixels() >=10:
-                self.pixels.setPixelColor((currentPixel+self.pixels.numPixels()-ahead)%self.pixels.numPixels(),self.pixels.Color(color[0]*divisor,color[1]*divisor,color[2]*divisor))
